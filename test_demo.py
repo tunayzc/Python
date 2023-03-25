79a5c8e9eed1039bba74c0117eec9f0d5d0a4e80
tunayzc <120184420+tunayzc@users.noreply.github.com>
2023-03-25 17:44:05 +0300

    Screenshot

--- test_demo.py
+++ test_demo.py
@@ -5,6 +5,9 @@ from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as ec
 from selenium.webdriver.common.action_chains import ActionChains 
 import pytest
+from pathlib import Path
+from datetime import date
+
 #prefix == ön ek test
 #postfix 
 class Test_DemoClass:
@@ -13,6 +16,9 @@ class Test_DemoClass:
         self.driver = webdriver.Chrome()
         self.driver.maximize_window()
         self.driver.get("https://www.saucedemo.com/")
+        #günün tarihini alıp bu tarih ile klasör var mı kontrol et yoksa oluştur
+        self.folderPath= str(date.today())
+        Path(self.folderPath).mkdir(exist_ok=True)
     #---testten sonra---
     def teardown_method(self):
         self.driver.quit()
@@ -36,6 +42,7 @@ class Test_DemoClass:
         loginButton = self.driver.find_element(By.ID, "login-button")
         loginButton.click()
         errorMessage = self.driver.find_element(By.XPATH,"//*[@id='login_button_container']/div/form/div[3]/h3")
+        self.driver.save_screenshot(f"{self.folderPath}/test-invalid-login-{username}-{password}.png")
         assert errorMessage.text =="Epic sadface: Username and password do not match any user in this service"
  
     
