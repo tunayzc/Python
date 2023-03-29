bf203754569bd639884838b67d4912d397a40931
tunayzc <120184420+tunayzc@users.noreply.github.com>
2023-03-29 20:58:50 +0300

    29.03

--- test_invalidlogin.py
+++ test_invalidlogin.py
@@ -10,7 +10,7 @@ from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
-class TestInvalidlogin():
+class Test_Invalidlogin():
   def setup_method(self, method):
     self.driver = webdriver.Chrome()
     self.vars = {}
